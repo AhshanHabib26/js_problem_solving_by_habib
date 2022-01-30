14e72928724081595dfcec3c604fee2214da9679
AhshanHabib26 <habib.net27@gmail.com>
2022-01-30 22:40:37 +0600

    Convert Celsius To Fahrenheit

---
+++ celsiusToFahrenheit.js
@@ -0,0 +1,23 @@
+// Convert Celsius To Fahrenheit 
+// Formula  °F = °C × (9/5) + 32.
+
+
+
+function getFahrenheit(n){
+    let result = n * (9/5) + 32;
+    return "Your Fahrenheit Is A " + " : " + result
+}
+console.log(getFahrenheit(20))
+
+
+
+// Another Idea To Find Out Celsius To Fahreheit 
+// Formula °F = °C * 1.8000 + 32.00
+
+
+function getFahrenheit(n){
+    let result = n * 1.8000 + 32.00;
+    return "Your Fahrenheit Is A " + " : " + result
+}
+
+console.log(getFahrenheit(20))
